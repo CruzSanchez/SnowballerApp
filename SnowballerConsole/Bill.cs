cc704ded9862617eef1043d4b357391ad7e0abeb
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-03-05 23:00:51 -0600

    Base program working, calculating individual payoffs. Need to add rest of the functionality

---
+++ SnowballerConsole/Bill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnowballerConsole
+{
+    public class Bill
+    {
+        public string Name { get; set; }
+        public decimal Total { get; set; }
+        public decimal MonthlyPayment { get; set; }
+
+        private DateTime _monthsPayOff;
+
+        public DateTime MonthsPayOff
+        {
+            get { return _monthsPayOff; }            
+        }
+        
+        public Bill(string name, decimal total, decimal monthlyPayment)
+        {
+            Name = name;
+            Total = total;
+            MonthlyPayment = monthlyPayment;
+            _monthsPayOff = DateTime.Now;
+        }
+
+        public DateTime GetPayOff(decimal total, decimal monthlyPayment)
+        {
+            var months = Convert.ToInt32(total / monthlyPayment);
+            _monthsPayOff = _monthsPayOff.AddMonths(months);
+            return _monthsPayOff;
+        }
+    }
+}
--- SnowballerConsole/Program.cs
+++ SnowballerConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SnowballerConsole
 {
@@ -6,7 +7,43 @@ namespace SnowballerConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cont = false;
+            List<Bill> bills = new List<Bill>();
+
+            do
+            {
+                Console.WriteLine("What bill would you like to add? It should be in this format seperated by a comma: {name},{total},{monthly payment}");
+                var billInfo = Console.ReadLine();
+                try
+                {
+                    var split = billInfo.Split(",");
+                    Bill bill = new Bill(split[0], decimal.Parse(split[1]), decimal.Parse(split[2]));
+                    bills.Add(bill);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine();
+                    Console.WriteLine(e.StackTrace);
+                }
+
+                Console.WriteLine("Add another bill? y/n");
+                var userResponse = Console.ReadLine().ToLower();
+
+                if (userResponse == "y")
+                    cont = true;
+                else
+                    cont = false;
+
+            } while (cont);
+
+            foreach (var bill in bills)
+            {
+                bill.GetPayOff(bill.Total,bill.MonthlyPayment);
+                var payoff = bill.MonthsPayOff.ToShortDateString();
+                Console.WriteLine($"Your pay off date for {bill.Name} is {payoff}");
+            }
+
         }
     }
 }
